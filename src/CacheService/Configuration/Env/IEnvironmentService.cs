893cd1a8f459158a204adfd419e823ef9db2ff01
Michal Moudrý <moudry.michal@live.com>
2024-04-21 17:19:22 +0200

    Added env service interface

---
+++ src/CacheService/Configuration/Env/IEnvironmentService.cs
@@ -0,0 +1,13 @@
+﻿namespace CacheService.Configuration.Env;
+
+/// <summary>
+/// A service for handling cache's environments.
+/// </summary>
+internal interface IEnvironmentService
+{
+    /// <summary>
+    /// Cache's current environment, specified by
+    /// a cmd arg, env variable or default (based on build configuration).
+    /// </summary>
+    Environment CurrentEnvironment { get; }
+}
