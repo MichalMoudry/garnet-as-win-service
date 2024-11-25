062b9a59412b6723ac57823e05d35b3c302dc837
Michal Moudrý <moudry.michal@live.com>
2024-11-25 14:08:21 +0100

    updated test client + updated garnet

--- src/CacheService.TestClient/BasicCacheTests.fs
+++ src/CacheService.TestClient/BasicCacheTests.fs
@@ -1,13 +1,13 @@
 module CacheService.TestClient
 
 open System
+open System.Collections.Generic
 open System.Text.Json
 open System.Text.Json.Serialization
 open NUnit.Framework
 open StackExchange.Redis
 
 /// A simple structure for testing different data types in JSON.
-[<Struct>]
 type TestData = {
     [<JsonPropertyName("string")>]
     TestString: string
@@ -23,14 +23,14 @@ let mutable cache: option<IDatabase> = None
 let Setup () =
     task {
         let! conn = ConnectionMultiplexer.ConnectAsync(
-            "localhost:6379,password=AxKZn7WuI.2dB6dp5|1z"
+            "127.0.0.1:6379,password=AxKZn7WuI.2dB6dp5|1z,abortConnect=false"
         )
         cache <- Some(conn.GetDatabase())
     }
 
 /// A test case covering a simple write and read from the cache.
 [<TestCase("testKey", "test_value")>]
-[<Ignore("For local dev")>]
+//[<Ignore("For local dev")>]
 let TestStringKeySetAndRead (key: string, value: string) =
     let uniqueKey = $"{key}_{Guid.NewGuid()}"
 
@@ -44,7 +44,7 @@ let TestStringKeySetAndRead (key: string, value: string) =
 
 /// A test case covering handling of JSON content.
 [<Test>]
-[<Ignore("For local dev")>]
+//[<Ignore("For local dev")>]
 let TestJsonValueSetAndRead () =
     let uniqueKey = $"test_val_{Guid.NewGuid()}"
     let inputData = {
@@ -65,3 +65,28 @@ let TestJsonValueSetAndRead () =
         Assert.That(setResult, Is.True)
         Assert.That(cacheContent, Is.EqualTo(inputData));
     ))
+
+[<Test>]
+let TestListOps () =
+    task {
+        let key = $"test_list_{Guid.NewGuid()}"
+        let input = {
+            TestString = "test string"
+            TestInt = 5
+            TestDate = DateTimeOffset.Now
+        }
+        let content = JsonSerializer.Serialize(input)
+
+        let! id1 = cache.Value.ListLeftPushAsync(key, content)
+        Assert.That(id1, Is.EqualTo(1))
+
+        let! id2 = cache.Value.ListLeftPushAsync(key, content)
+        Assert.That(id2, Is.EqualTo(2))
+
+        let! valuesRange = cache.Value.ListRangeAsync(key)
+        let values = List<TestData>(valuesRange.Length)
+        for value in valuesRange do
+            values.Add(JsonSerializer.Deserialize(value))
+
+        Assert.That(values, Does.Contain(input))
+    }
